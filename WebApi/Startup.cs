1363226357525f0a829d0c76c3b2b6ce713db449
Ahmet Eren Kardaş <erenkardas@gmail.com>
2022-08-05 15:12:20 +0300

    authorization ve jwt yazıldı

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -4,7 +4,7 @@ using Core.DependencyResolvers;
 using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
-using Core.Utilities.Security.JWT;
+
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -13,92 +13,82 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using System.Linq;
 
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.IdentityModel.Tokens;
+using Core.Utilities.Security.JWT;
 
 namespace WebAPI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-
-            services.AddSingleton<IFisService, FisManager>();
-            services.AddSingleton<IFisDal, EfFisDal>();
-
-            services.AddSingleton<IMakbuzService, MakbuzManager>();
-            services.AddSingleton<IMakbuzDal, EfMakbuzDal>();
-
-            services.AddSingleton<IUserService, UserManager>();
-            services.AddSingleton<IUserDal, EfUserDal>();
-
-            services.AddSingleton<IAuthService, AuthManager>();
-            services.AddSingleton<ITokenHelper, JwtHelper>();
-
-
-
-            services.AddCors();
-
-            var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
-
-            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            //    .AddJwtBearer(options =>
-            //    {
-            //        options.TokenValidationParameters = new TokenValidationParameters
-            //        {
-            //            ValidateIssuer = true,
-            //            ValidateAudience = true,
-            //            ValidateLifetime = true,
-            //            ValidIssuer = tokenOptions.Issuer,
-            //            ValidAudience = tokenOptions.Audience,
-            //            ValidateIssuerSigningKey = true,
-            //            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
-            //        };
-            //    });
-
-
-
-            services.AddDependencyResolvers(new ICoreModule[] { new CoreModule() });
-        }
-
-
-
-
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            public Startup(IConfiguration configuration)
             {
-                app.UseDeveloperExceptionPage();
+                Configuration = configuration;
             }
 
-            app.ConfigureCustomExceptionMiddleware();
+            public IConfiguration Configuration { get; }
 
-            app.UseCors(builder => builder.WithOrigins("https://localhost:44340").AllowAnyHeader());
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            // This method gets called by the runtime. Use this method to add services to the container.
+            public void ConfigureServices(IServiceCollection services)
             {
-                endpoints.MapControllers();
+                //AOP
+                //Autofac, Ninject,CastleWindsor, StructureMap, LightInject, DryInject -->IoC Container
+                //AOP
+                //Postsharp
+                services.AddControllers();
+                //services.AddSingleton<IProductService,ProductManager>();
+                //services.AddSingleton<IProductDal, EfProductDal>();
+
+                services.AddCors();
+
+                var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidIssuer = tokenOptions.Issuer,
+                            ValidAudience = tokenOptions.Audience,
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+                        };
+                    });
+
+                services.AddDependencyResolvers(new ICoreModule[] {
+               new CoreModule()
             });
+
+            }
+
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            {
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+                app.ConfigureCustomExceptionMiddleware();
+
+                app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
+
+                app.UseHttpsRedirection();
+
+                app.UseRouting();
+
+                app.UseAuthentication();
+
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+                //23.10 dersteyiz
+            }
         }
-    }
 }
